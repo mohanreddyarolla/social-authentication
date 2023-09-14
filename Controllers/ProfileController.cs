4b190993a0e75a49aae982a2a6ad7a8d1bf61559
mohan-reddy-arolla <mohan.a@technovert.com>
2023-09-14 16:09:07 +0530

    Add project files.

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace webapi.Controllers
+{
+    [Authorize]
+    [RequiredScope("read")]
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+
+            var email = HttpContext.User.FindFirst(ClaimTypes.Name)?.Value;
+
+            var userData = new
+            {
+                Email = email,
+
+                // Other user data properties
+            };
+
+            return Ok(userData);
+
+        }
+    }
+}
